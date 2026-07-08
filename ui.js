1727ed8a6cb87fc39007ddc051798edcd4e9024a
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2026-07-08 15:36:15 +1200

    Completed connected buyTicket button to the js. Created function to generate new ticket elements, which kept the design elements from my prototype. Also correctly updates the sidebar.

--- ui.js
+++ ui.js
@@ -29,6 +29,82 @@ function updateSidebar(state){
     /*drawDate.innerText = `${state.drawDate}`;
     prizeBreakdown.innerText = `${state.prizeBreakdown}`;*/
 }
+/*
+function updateTickets(tickets){
+    playerTickets.innerHTML = '';
+    tickets.forEach((ticket, index) => {
+        renderTicket(ticket, index);
+    });
+}*/
+
+function addTicketToUI(ticket, index){
+    const ticketElement = renderTicket(ticket, index);
+    playerTickets.prepend(ticketElement);
+}
+
+function renderTicket(ticket, index){
+    const ticketElement = document.createElement("div");
+    ticketElement.classList.add("ticket");
+
+    const ticketContainer = document.createElement("div");
+    ticketContainer.classList.add("ticketContainer");
+
+    const ticketNumber = document.createElement("h4");
+    ticketNumber.innerText = `Ticket ${index + 1}`;
+
+    const ticketBallsElement = document.createElement("div");
+    ticketBallsElement.classList.add("ticketBalls");
+    
+    for(let i = 0; i < ticket.length-1; i++){
+        const ballElement = document.createElement("div");
+        ballElement.classList.add("ball");
+        ballElement.innerText = ticket[i];
+        ticketBallsElement.appendChild(ballElement);
+    }
+    const powerBallSection = document.createElement("div");
+    powerBallSection.classList.add("ballSection", "powerball");
+
+    const powerBall = document.createElement("div");
+    powerBall.classList.add("ball", "powerball");
+    powerBall.innerText = ticket[ticket.length-1];
+
+    powerBallSection.appendChild(powerBall);
+    ticketBallsElement.appendChild(powerBallSection);
+
+    const ticketEnd = document.createElement("div");
+    ticketEnd.classList.add("ticketEnd");
+
+    const matchedNum = document.createElement("div");
+    matchedNum.classList.add("matchedNum");
+    matchedNum.innerText = `? matched`;
+
+    const ticketCost = document.createElement("div");
+    ticketCost.classList.add("ticketCost");
+    ticketCost.innerText = `$2`;
+    
+    ticketEnd.appendChild(matchedNum);
+    ticketEnd.appendChild(ticketCost);
+
+    ticketContainer.appendChild(ticketNumber);
+    ticketContainer.appendChild(ticketBallsElement);
+    
+    ticketElement.appendChild(ticketContainer);
+    ticketElement.appendChild(ticketEnd);
+
+    return ticketElement;
+
+}
+
+buyButton.addEventListener("click", () => {
+    if (player.getMoney() < game.getPrice()) return;
+    player.buyTicket(game.getPrice());
+    const tickets = player.getTickets();
+    addTicketToUI(tickets[tickets.length - 1], tickets.length -1);
+    updateUI();
+})
+
+//Next is playButton.
+
 
 function updateUI(){
     const state = game.getGameState(player);
